6b9116082239e1d08c608e7d0b021fb6c97e397f
Sakura <Sakura@qq.com>
2021-11-06 11:31:28 +0800

    push commit

--- src/main/java/com/hk/git/GitTest.java
+++ src/main/java/com/hk/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git_hot-fix1！");
         System.out.println("hello git_hot-fix2！");
         System.out.println("hello git_hot-fix3！");
+        System.out.println("push test");
     }
 }
